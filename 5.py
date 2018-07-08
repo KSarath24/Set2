222016d54b7040702aca4f93af43f65e7cedc00e
K Sarath Babu <40817448+KSarath24@users.noreply.github.com>
2018-07-08 21:58:26 +0530

    Update 5.py

--- 5.py
+++ 5.py
@@ -1,5 +1,5 @@
-lower = int(input("Enter the lower limit : "))
-upper = int(input("Enter the upper limit : "))
-for i in range(lower,upper+1):
-  if(i%2 == 0):
-    print(i)
+lower = int(input("Enter lower limit : "))
+upper = int(input("Enter upper limit : "))
+for s in range(lower,upper+1):
+  if(s%2 == 0):
+    print(s)
